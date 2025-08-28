37d2174b2c10473572f7f1d95de9cfd5108d4afb
Cirillo Negruzzi <assus.12345nagi@gmail.com>
2025-08-28 19:37:22 +0200

    make build

---
+++ landing-page-portfolio.46da10d0.js
@@ -0,0 +1,2 @@
+"use strict";new WOW().init();
+//# sourceMappingURL=landing-page-portfolio.46da10d0.js.map
---
+++ landing-page-portfolio.f75de5e1.js
@@ -0,0 +1,4 @@
+'use strict';
+new WOW().init();
+
+//# sourceMappingURL=landing-page-portfolio.f75de5e1.js.map
